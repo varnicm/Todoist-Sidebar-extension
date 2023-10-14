dc723df101420401524c769d0035987016a46df8
Moe Sanjari <moe.sanjari@gmail.com>
2023-10-14 02:46:05 -0500

    return back data in array in background.js

--- background.js
+++ background.js
@@ -1,20 +1,37 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'fetchTodoistData') {
-        const token = request.token;
-        const headers = new Headers({
-            "Authorization": `Bearer ${token}`
+    if (request.action === "fetchTasks") {
+        fetchTodoistTasks(request.token).then(tasks => {
+            sendResponse({ success: true, data: tasks });
+        }).catch(error => {
+            sendResponse({ success: false, error: error.toString() });
         });
 
-        fetch('https://api.todoist.com/rest/v1/tasks', { headers: headers })
-            .then(response => response.json())
-            .then(data => {
-                sendResponse({ data: data });
-            })
-            .catch(error => {
-                sendResponse({ error: error.toString() });
-            });
-
-        // This will keep the message channel open until `sendResponse` is called
+        // This is important: Return true to indicate that you wish to send a response asynchronously
         return true;
     }
 });
+
+function fetchTodoistTasks(token) {
+    return new Promise((resolve, reject) => {
+        const endpoint = 'https://api.todoist.com/rest/v2/projects';
+        fetch(endpoint, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+            resolve(data);
+        })
+        .catch(error => {
+            reject(error);
+        });
+    });
+}
