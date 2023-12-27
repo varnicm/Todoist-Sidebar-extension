3459b280f0d7e1ebea58ff2c8d34a5ad531cd322
Moe Sanjari <sanjari@gmail.com>
2023-12-27 07:13:47 -0800

    first working version, displays all project

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+function fetchTodoistProjects(token) {
+    return fetch("https://api.todoist.com/rest/v2/projects", {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(projects => {
+        console.log(projects);
+        return projects; // This contains the list of projects
+    })
+    .catch(error => {
+        console.error('Failed to fetch projects:', error);
+    });
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     const button = document.getElementById('loginBtn');
     var token = localStorage.getItem('todoistToken');
@@ -32,6 +54,22 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
+// window.addEventListener("message", function(event) {
+//     if (event.data.messageType && event.data.messageType === 'authorizationToken') {
+//         var token = event.data.token;
+//         console.log("Received token:", token);
+
+//         // Save the token for future use
+//         localStorage.setItem('todoistToken', token);
+
+//         // Update the UI
+//         document.getElementById('status').innerText = 'Logged in';
+//         document.getElementById('loginBtn').style.display = 'none';
+
+//         // Optionally, fetch tasks or other data here using the token.
+//     }
+// });
+
 window.addEventListener("message", function(event) {
     if (event.data.messageType && event.data.messageType === 'authorizationToken') {
         var token = event.data.token;
@@ -44,9 +82,27 @@ window.addEventListener("message", function(event) {
         document.getElementById('status').innerText = 'Logged in';
         document.getElementById('loginBtn').style.display = 'none';
 
-        // Optionally, fetch tasks or other data here using the token.
+        // Fetch Todoist projects
+        fetchTodoistProjects(token)
+            .then(projects => {
+                if (projects) {
+                    // TODO: Call a function to handle and display these projects
+                    displayProjects(projects);
+                }
+            });
     }
 });
+
+function displayProjects(projects) {
+    const projectsList = document.getElementById('tasksList');
+    projectsList.innerHTML = ''; // Clear any existing content
+
+    projects.forEach(project => {
+        const projectElement = document.createElement('div');
+        projectElement.innerText = project.name; // Assuming 'name' is a property of your project
+        projectsList.appendChild(projectElement);
+    });
+}
 
 function fetchTasks(token) {
     // Here, call the Todoist API to fetch tasks or other information using the token.
@@ -68,8 +124,7 @@ function fetchUserTasks(token) {
     });
 }
 
+
+
 // Usage
 const accessToken = localStorage.getItem('todoistToken');  // Replace with your token
-
-
-
